94d2073cab0952d865a45ac5bd0701a1783c1684
seagraves <christopher.seagraves@gmail.com>
2026-09-11 18:21:23 -0500

    write a test for 0 velocity and assert it's valid

--- telemetry/tests/test.py
+++ telemetry/tests/test.py
@@ -1,3 +1,9 @@
 from telemetry.analysis import Analysis
 from telemetry.reading import Reading
 from telemetry.robot import Robot
+from telemetry.validator import Validator
+
+
+def test_zero_velocity_line():
+    validator, value = Validator.validate_velocity("0.0")
+    assert validator.is_valid
