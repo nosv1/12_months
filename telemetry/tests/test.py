ab3454c29b8e93c027ff56794ec929e53df2254a
seagraves <christopher.seagraves@gmail.com>
2026-09-12 14:19:22 -0500

    update test_zero_velocity_line to use new validator logic

--- telemetry/tests/test.py
+++ telemetry/tests/test.py
@@ -1,9 +1,14 @@
 from telemetry.analysis import Analysis
 from telemetry.reading import Reading
 from telemetry.robot import Robot
-from telemetry.validator import Validator
+from telemetry.validator import validate_velocity
 
 
 def test_zero_velocity_line():
-    validator, value = Validator.validate_velocity("0.0")
-    assert validator.is_valid
+    try:
+        value = validate_velocity("0.0")
+
+    except ValueError as ve:
+        assert str(ve) != ""
+
+    assert value == 0.0
