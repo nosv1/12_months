464d5d8a72fd26f8573f5678287d29f4a54bc0ff
seagraves <christopher.seagraves@gmail.com>
2026-09-12 22:10:15 -0500

    redesign parser to separate into a single job, parse a line into single string values
    parser now returns ParsedLine, that ParsedLine is then attempted to be validated and so on from there
    
    parse_telemetry_lines now loops the lines and sends one line at a time to ParsedLine.parse_line()

--- telemetry/src/telemetry/cli.py
+++ telemetry/src/telemetry/cli.py
@@ -1,76 +1,113 @@
 import argparse
 import json
 import logging
-from enum import Enum
+from collections.abc import Iterable
 from pathlib import Path
-from typing import TextIO
+from typing import Optional
 
 from telemetry.analysis import Analysis
 from telemetry.reading import Reading
 from telemetry.robot import Robot
+from telemetry.telemetry_warning import BatteryWarning, TemperatureWarning
 from telemetry.validator import (
     validate_battery,
     validate_temperature,
     validate_timestamp,
     validate_velocity,
 )
-from telemetry.telemetry_warning import BatteryWarning, TemperatureWarning
 
 logger = logging.getLogger(__name__)
 
 
-def parser(telemetry_lines: list[str]) -> dict[str, Robot]:
-    class HEADERS(Enum):
-        TIMESTAMP_IDX = 0
-        ROBOT_ID_IDX = 1
-        VELOCITY_IDX = 2
-        BATTERY_IDX = 3
-        TEMPERATURE_IDX = 4
-        NUM_COLUMNS = 5
+class ParsedLine:
+    TIMESTAMP_IDX = 0
+    ROBOT_ID_IDX = 1
+    VELOCITY_IDX = 2
+    BATTERY_IDX = 3
+    TEMPERATURE_IDX = 4
+    NUM_COLUMNS = 5
+
+    def __init__(
+        self,
+        timestamp_str: str,
+        robot_id: str,
+        velocity_str: str,
+        battery_str: str,
+        temperature_str: str,
+    ):
+        self.timestamp_str = timestamp_str
+        self.robot_id = robot_id
+        self.velocity_str = velocity_str
+        self.battery_str = battery_str
+        self.temperature_str = temperature_str
+
+    @staticmethod
+    def parse_line(line: str):
+        parts = line.split(",")
+        if len(parts) != ParsedLine.NUM_COLUMNS:
+            raise BufferError("line is missing data")
+
+        return ParsedLine(
+            timestamp_str=parts[ParsedLine.TIMESTAMP_IDX],
+            robot_id=parts[ParsedLine.ROBOT_ID_IDX],
+            velocity_str=parts[ParsedLine.VELOCITY_IDX],
+            battery_str=parts[ParsedLine.BATTERY_IDX],
+            temperature_str=parts[ParsedLine.TEMPERATURE_IDX],
+        )
+
+
+def validate_parsed_line(
+    parsed_line: ParsedLine, prev_reading: Optional[Reading]
+) -> Reading:
+    return Reading(
+        robot_id=parsed_line.robot_id,
+        timestamp=validate_timestamp(
+            parsed_line.timestamp_str,
+            prev_reading.timestamp if prev_reading else None,
+        ),
+        velocity=validate_velocity(parsed_line.velocity_str),
+        battery=validate_battery(parsed_line.battery_str),
+        temperature=validate_temperature(parsed_line.temperature_str),
+    )
+
+
+def parse_telemetry_lines(lines: Iterable[str], headers=0) -> dict[str, Robot]:
+    """lines should not include the header -- the first line should be
+    the first parseable line"""
 
     # defining with unknown in case there is missing data in a telemetry line
     # it may be the case where robot id *is* inside the line, but it's also less
     # easy to assume robot id is in the correct index if there is missing information
     robots_dict: dict[str, Robot] = {"unknown": Robot("unknown")}
 
-    for i, line in enumerate(telemetry_lines):
-        if not i:
+    for i, line in enumerate(lines):
+        if i < headers:
             continue
 
-        parts = line.split(",")
-        if not parts or len(parts) != HEADERS.NUM_COLUMNS.value:
+        try:
+            parsed_line = ParsedLine.parse_line(line)
+
+        except BufferError:
             logger.warning("Line %d was missing data.", i + 1)
             robots_dict["unknown"].bad_readings.append((line, "line is missing data"))
             continue
 
-        robot_id = parts[HEADERS.ROBOT_ID_IDX.value]
+        robot_id = parsed_line.robot_id
         if robot_id not in robots_dict:
             robots_dict[robot_id] = Robot(robot_id)
         robot = robots_dict[robot_id]
 
         try:
-            timestamp = validate_timestamp(
-                parts[HEADERS.TIMESTAMP_IDX.value],
-                robot.readings[-1].timestamp if robot.readings else None,
+            reading = validate_parsed_line(
+                parsed_line, robot.readings[-1] if robot.readings else None
             )
-            velocity = validate_velocity(parts[HEADERS.VELOCITY_IDX.value])
-            battery = validate_battery(parts[HEADERS.BATTERY_IDX.value])
-            temperature = validate_temperature(parts[HEADERS.TEMPERATURE_IDX.value])
 
         except ValueError as ve:
             logger.warning("Line %d had a value error - %s", i + 1, str(ve))
             robot.bad_readings.append((line, str(ve)))
             continue
 
-        robot.readings.append(
-            Reading(
-                robot_id=robot_id,
-                timestamp=timestamp,
-                velocity=velocity,
-                battery=battery,
-                temperature=temperature,
-            )
-        )
+        robot.readings.append(reading)
 
     return robots_dict
 
@@ -110,13 +147,15 @@ def main():
         TemperatureWarning(max_temperature=60),
     ]
     with telemetry_file_path.open("r") as telemetry_file:
-        robots_dict = parser(telemetry_file.readlines())
-        analysis_dict = {
-            r_id: Analysis.analyze_robot(r, defined_warnings)
-            for r_id, r in robots_dict.items()
-        }
-        # [r.plot(output_dir) for r in robots_dict.values()]
-        report(output_dir, analysis_dict)
+        telemetry_lines: Iterable[str] = telemetry_file.readlines()
+        robots_dict = parse_telemetry_lines(telemetry_lines, headers=1)
+
+    analysis_dict = {
+        r_id: Analysis.analyze_robot(r, defined_warnings)
+        for r_id, r in robots_dict.items()
+    }
+    # [r.plot(output_dir) for r in robots_dict.values()]
+    report(output_dir, analysis_dict)
 
 
 if __name__ == "__main__":
