13ccee04c71a189010dd74af1599a4a2c7702b8f
seagraves <christopher.seagraves@gmail.com>
2026-09-12 21:14:38 -0500

    update zero_velocity test into two tests
    one test for 0.0 being valid and the other for the exception being raised when there is an invalid velocity (nan currently tested, still need the other failures)

--- telemetry/tests/test_validations.py
+++ telemetry/tests/test_validations.py
@@ -1,3 +1,5 @@
+import pytest
+
 from telemetry.analysis import Analysis
 from telemetry.reading import Reading
 from telemetry.robot import Robot
@@ -5,10 +7,9 @@ from telemetry.validator import validate_velocity
 
 
 def test_zero_velocity_line():
-    try:
-        value = validate_velocity("0.0")
+    assert validate_velocity("0.0") == 0.0
 
-    except ValueError as ve:
-        assert str(ve) != ""
 
-    assert value == 0.0
+def test_nan_velocity():
+    with pytest.raises(ValueError):
+        validate_velocity("nan")
