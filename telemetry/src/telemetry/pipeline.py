e5a0a28b6572fa675659d008cc1e1b930c6d6343
seagraves <christopher.seagraves@gmail.com>
2026-09-16 18:50:53 -0500

    remove header_count from parse lines
    left over change from ColumnCountError commit

--- telemetry/src/telemetry/pipeline.py
+++ telemetry/src/telemetry/pipeline.py
@@ -10,7 +10,7 @@ from telemetry.validator import validate_parsed_robots
 def analyze_telemetry(
     telemetry_lines: Iterable[str], defined_warnings: list[TelemetryWarning]
 ) -> Analysis:
-    parsed_lines, bad_readings = parse_lines(telemetry_lines, headers_count=1)
+    parsed_lines, bad_readings = parse_lines(telemetry_lines)
     parsed_robots = group_robots_as_parsed_lines(parsed_lines)
     validated_robots = validate_parsed_robots(parsed_robots)
     robot_analyses = analyze_robots(validated_robots, defined_warnings)
