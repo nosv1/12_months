5ed7545ff066e70ed6dcd6804806ed72ae2220ef
seagraves <christopher.seagraves@gmail.com>
2026-09-18 19:39:24 -0500

    append the -> None for the exception inits

--- telemetry/src/telemetry/exceptions.py
+++ telemetry/src/telemetry/exceptions.py
@@ -4,23 +4,23 @@ from datetime import datetime
 
 
 class TelemetryException(Exception):
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         super().__init__(*args)
 
 
 class UnknownError(TelemetryException):
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         super().__init__(*args)
 
 
 class NotANumberError(TelemetryException):
-    def __init__(self, field: str, cause: str):
+    def __init__(self, field: str, cause: str) -> None:
         cause = cause if len(cause) > 0 else "[no value]"
         super().__init__(f"{field} was not number. - '{cause}'")
 
 
 class NaNError(TelemetryException):
-    def __init__(self, field: str, cause: str):
+    def __init__(self, field: str, cause: str) -> None:
         super().__init__(f"{field} was nan. - '{cause}'")
 
 
@@ -28,7 +28,7 @@ class NaNError(TelemetryException):
 
 
 class ColumnCountError(TelemetryException):
-    def __init__(self, expected: int, actual: int):
+    def __init__(self, expected: int, actual: int) -> None:
         super().__init__(f"Expected {expected} columns, found {actual}")
 
 
@@ -37,21 +37,21 @@ class ColumnCountError(TelemetryException):
 
 ###   TIMESTAMP   ###
 class TimestampOutOfOrderError(TelemetryException):
-    def __init__(self, timestamp: datetime, prev_timestamp: datetime):
+    def __init__(self, timestamp: datetime, prev_timestamp: datetime) -> None:
         super().__init__(
             f"The timestamp is earlier in time than the robot's previous timestamp - '{timestamp}' <= '{prev_timestamp}'"
         )
 
 
 class TimestampIdenticalError(TelemetryException):
-    def __init__(self, timestamp: datetime):
+    def __init__(self, timestamp: datetime) -> None:
         super().__init__(
             f"The timestamp matches the robot's previous timestamp - {timestamp}"
         )
 
 
 class TimestampFormatError(TelemetryException):
-    def __init__(self, timestamp_str: str):
+    def __init__(self, timestamp_str: str) -> None:
         super().__init__(
             f"The timestamp format does not match ISO 8601, UTC - {timestamp_str}"
         )
@@ -59,47 +59,47 @@ class TimestampFormatError(TelemetryException):
 
 ###   VELOCITY   ###
 class VelocityNotANumberError(NotANumberError):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(field="Velocity", cause=cause)
 
 
 class VelocityWasNaNError(NaNError):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(field="Velocity", cause=cause)
 
 
 class VelocityOutOfRangeError(TelemetryException):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(f"The velocity was out of the specified range - '{cause}'")
 
 
 ###   BATTERY   ###
 class BatteryNotANumberError(NotANumberError):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(field="Battery", cause=cause)
 
 
 class BatteryWasNaNError(NaNError):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(field="Battery", cause=cause)
 
 
 class BatteryOutOfRangeError(TelemetryException):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(f"Battery was out of the specified range - '{cause}'.")
 
 
 ###   TEMPERATURE   ###
 class TemperatureNotANumberError(NotANumberError):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(field="Temperature", cause=cause)
 
 
 class TemperatureWasNaNError(NaNError):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(field="Temperature", cause=cause)
 
 
 class TemperatureOutOfRangeError(TelemetryException):
-    def __init__(self, cause: str):
+    def __init__(self, cause: str) -> None:
         super().__init__(f"Temperature was out of the specified range - '{cause}'.")
