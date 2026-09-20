1be06f689a56d07215a2c7536134b0bb644f545b
seagraves <christopher.seagraves@gmail.com>
2026-09-20 09:14:56 -0500

    configure cli to handle arguments and run main
    positional arg: telemetry_path as Path
    optional arg: --log_info [sets logging level to info]

--- boss-fights/01-telemetry/telemetry_boss_fight/src/telemetry_boss_fight/cli.py
+++ boss-fights/01-telemetry/telemetry_boss_fight/src/telemetry_boss_fight/cli.py
@@ -1,5 +1,33 @@
 # types one command, points it at the file, and gets the report
 
+import argparse
+import logging
+from pathlib import Path
+
+from telemetry_boss_fight.main import main
+
+logger = logging.getLogger(__name__)
+
+
+def handle_logging_level(log_info: None | str):
+    if log_info is None:
+        return
+
+    logging.basicConfig(level=logging.INFO)
+    logger.info("Logging now set to INFO")
+
 
 def cli() -> None:
-    pass
+    parser = argparse.ArgumentParser(description="...")
+    parser.add_argument(
+        "telemetry_path",
+        help="the path to the telemetry file",
+        type=Path,
+    )
+    parser.add_argument(
+        "-l", "--log_info", help="set logging level to 'info'", action="store_true"
+    )
+
+    args = parser.parse_args()
+    handle_logging_level(args.log_info)
+    main(args.telemetry_path)
--- boss-fights/01-telemetry/telemetry_boss_fight/src/telemetry_boss_fight/reader.py
+++ boss-fights/01-telemetry/telemetry_boss_fight/src/telemetry_boss_fight/reader.py
@@ -1,6 +1,10 @@
+import logging
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
 
 
 def read_telemetry_file(telemetry_path: Path) -> list[str]:
+    logger.info("Reading %s...", str(telemetry_path))
     with open(telemetry_path, "r") as tf:
         return tf.readlines()
